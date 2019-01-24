b89d8b18591afe2eb3e0a4122f68b44e08c915f8
Anton Serdyuchenko <anton415@gmail.com>
2019-01-24 05:55:30 +0300

    Remove unused copy from index.js.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(React.createElement('h1', null, 'Hello from react!'),
-document.getElementById('root'));
-
 ReactDOM.render(<h1>Hello from react!</h1>),
 document.getElementById('root'));
