a4f6f99d1b8618eafca4f300b1ec67fc02904a3a
Anton Serdyuchenko <anton415@gmail.com>
2019-01-29 06:03:42 +0300

    Create and setting state.

--- src/components/LoginContainer.js
+++ src/components/LoginContainer.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
 class LoginContainer extends Component {
+  state = { email: '', password: '' }
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div id="LoginContainer" className="inner-container">
@@ -8,10 +23,20 @@ class LoginContainer extends Component {
           <img src="/assets/icon.png" alt="logo" />
           <h1>My project</h1>
         </div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <p>Sign in or sign up by entering your email and password.</p>
-          <input type="text" placeholder="Your email" />
-          <inpur type="password" placeholder="Your password" />
+          <input
+            type="text"
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+            placeholder="Your email"
+          />
+          <inpur
+            type="password"
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+            placeholder="Your password"
+          />
           <button className="red light" type="submit">Login</button>
         </form>
       </div>
