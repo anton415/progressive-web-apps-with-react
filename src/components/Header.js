a67ee09698129d1fab4a3ef3d1e940bc09d8ac25
Anton Serdyuchenko <anton415@gmail.com>
2019-01-30 05:22:22 +0300

    Create second componet (Header) and import them in first component (LoginPage).

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div id="Header">
+      <img src="/assets/icon.png" alt="logo" />
+      <h1>My project</h1>
+    </div>
+  );
+};
+
+
+export default Header;
